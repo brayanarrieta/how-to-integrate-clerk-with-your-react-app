45930a8f0693d45ac42a8a8efd27938003dae363
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2021-07-06 12:00:17 -0600

    Apply changes after feedback from clerk

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React, { useEffect } from "react";
-import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
-import { CLERK_FRONTEND_API, CLERK_SIGN_IN_URL } from "./constants";
+import React from "react";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn
+} from "@clerk/clerk-react";
+import { CLERK_FRONTEND_API } from "./constants";
 import Header from "./Header";
 import Home from "./Home";
-
-const RedirectToSignIn = () => {
-  useEffect(() => {
-    window.location.href = CLERK_SIGN_IN_URL;
-  });
-  return null;
-};
 
 const App = () => (
   <ClerkProvider frontendApi={CLERK_FRONTEND_API}>
